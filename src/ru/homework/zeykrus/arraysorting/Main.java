6f642e1c8835ed2504cca05c3615f3b0c970fb8d
Lev Vylegzhanin <levavil97@gmail.com>
2022-07-26 12:50:10 +1000

    Init ArraySorting task

---
+++ src/ru/homework/zeykrus/arraysorting/Main.java
@@ -0,0 +1,30 @@
+package ru.homework.zeykrus.arraysorting;
+
+//1.1. Реализуйте алгоритм сортировки пузырьком для сортировки массива
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[30];
+        for (int i = 0; i < array.length; i++) {
+            array[i]=(int)(Math.random()*100);
+        }
+        sort(array);
+        for (int i : array) {
+            System.out.print(i+" ");
+        }
+    }
+
+    private static void sort(int[] array) {
+        if(array!=null) {
+            for (int i = array.length; i > 0; i--) {
+                for (int j = 1; j < i; j++) {
+                    int temp = array[j];
+                    if(array[j-1]>array[j]) {
+                        array[j]=array[j-1];
+                        array[j-1]=temp;
+                    }
+                }
+            }
+        }
+    }
+}
